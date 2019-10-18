751c31ee81ccd79cb40bc0782a75ef7b0164b58f
hdpulido10 <56325161+hdpulido10@users.noreply.github.com>
2019-10-18 01:26:40 -0500

    Update Centro_Control.v

--- rtl/Centro_Control.v
+++ rtl/Centro_Control.v
@@ -53,6 +53,7 @@ parameter OUT_CSAI = 11;
 parameter DATAWIDTH_COND = 3;
 parameter DATAWIDTH_ADRR = 11;
 parameter DATAWIDTH_SELECTION_LENG = 2;
+parameter DATAWIDTH_MIR = 41;
 
 //=======================================================
 //  PORT declarations
@@ -96,6 +97,7 @@ wire [DATAWIDTH_COND-1:0] COND_MIR_CBL_CABLE;
 wire [DATAWIDTH_ADRR-1:0] JUMP_ADRR_MIR_CSMUX_CABLE;
 wire [DATAWIDTH_SELECTION_LENG-1:0] selector_CBL_CSMUX_CABLE;
 wire [DATAWIDTH_ALU-1:0] FLAGS_PSR_CBL_CABLE;
+wire [DATAWIDTH_MIR-1:0] ROM_TO_MIR;
 //=======================================================
 //  Structural coding
 //=======================================================
@@ -143,7 +145,7 @@ CS_MIR Centro_Control_CS_MIR (
 	.CS_MIR_RESET_InHigh(Centro_Control_RESET_InHigh),
 	.CS_MIR_clear_InLow(Centro_Control_clear_InLow),
 	.CS_MIR_load_InLow(Centro_Control_load_InLow),
-	.CS_MIR_INSTRUCTION_data_InBUS(JUMP_ADRR_CSAI_CSMUX_CABLE)
+	.CS_MIR_INSTRUCTION_data_InBUS(ROM_TO_MIR)
 );
 
 CC_PSR #( .DATAWIDTH_ALU_SELECTION(DATAWIDTH_ALU)) Centro_Control_PSR (
@@ -157,6 +159,14 @@ CC_PSR #( .DATAWIDTH_ALU_SELECTION(DATAWIDTH_ALU)) Centro_Control_PSR (
 	.CC_zero(Centro_Control_ALU_zero_Low),
 	.CC_PSR_CLOCK_50(Centro_Control_CLOCK_50)
 );
-endmodule
 
+CS_MIR Centro_Control_CS_MIR (
+	// port map - connection between master ports and signals/registers
+	// ---- Salidas -------------
+	.ROM_DataBUS_Out(ROM_TO_MIR),
+	// ---- Entradas ------------
+	.ROM_DataBUS_In(JUMP_ADRR_CSAI_CSMUX_CABLE)
+);
+
+endmodule
 
